fa946f32fd398667a088a1a379022e58732fea39
I-biubiubiu <273998074@qq.com>
2019-03-11 09:36:51 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('cart', 'CartController@index')->name('cart.index');
     // 购物车删除
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+    // 订单保存
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
