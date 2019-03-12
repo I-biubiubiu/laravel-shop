8989222c2e9e25bede23134d39ee67ec2167b3ee
I-biubiubiu <273998074@qq.com>
2019-03-12 16:22:43 +0800

    完善支付后逻辑

--- routes/web.php
+++ routes/web.php
@@ -46,8 +46,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
     // 支付宝支付
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    // 支付宝前端回调
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 });
 
+// 支付宝服务器回调
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
